7c6f3892738af3f35b9c68314f984353b9151eef
Matheus Alves <33577238+matheusphalves@users.noreply.github.com>
2024-08-28 10:59:50 -0300

    fix: adjust load balancer

--- code/modules/load_balancer/main.tf
+++ code/modules/load_balancer/main.tf
@@ -2,11 +2,15 @@ resource "google_compute_backend_service" "backend_service" {
   name                  = var.backend_service_name
   protocol              = "HTTP"
   port_name             = "http"
+  load_balancing_scheme = "EXTERNAL"
+  timeout_sec           = 10
+  enable_cdn            = false
   health_checks         = [google_compute_health_check.http_health_check.id]
-  load_balancing_scheme = "EXTERNAL_MANAGED"
-
   backend {
-    group = var.webserver_instance_group_self_link
+    group           = var.webserver_instance_group_self_link
+    balancing_mode  = "UTILIZATION"
+    max_utilization = 0.8
+    capacity_scaler = 1.0
   }
 }
 
@@ -17,7 +21,7 @@ resource "google_compute_health_check" "http_health_check" {
   healthy_threshold   = 2
   unhealthy_threshold = 2
 
-  tcp_health_check {
+  http_health_check {
     port = var.http_health_check_port
   }
 }
@@ -28,16 +32,17 @@ resource "google_compute_target_http_proxy" "target_http_proxy" {
 }
 
 resource "google_compute_url_map" "url_map" {
-  name            = "${var.load_balancer_name}"
+  name            = "${var.load_balancer_name}-url-map"
   default_service = google_compute_backend_service.backend_service.id
 }
 
 resource "google_compute_global_forwarding_rule" "default" {
-  name        = "${var.load_balancer_name}-forwarding-rule"
-  ip_protocol = "TCP"
-  port_range  = "80"
-  target      = google_compute_target_http_proxy.target_http_proxy.id
-  ip_address  = google_compute_global_address.global_address.address
+  name                  = "${var.load_balancer_name}-forwarding-rule"
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "80"
+  target                = google_compute_target_http_proxy.target_http_proxy.id
+  ip_address            = google_compute_global_address.global_address.address
 }
 
 resource "google_compute_global_address" "global_address" {
